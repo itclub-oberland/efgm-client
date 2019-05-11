f9aa320343eb8d9d5a88d9ca2ee2637c09a56319
javaangular1 <javaangular1@gmail.com>
2019-05-11 11:59:07 +0200

    test suite file creation

---
+++ e2e_tests.js
@@ -0,0 +1,18 @@
+describe('EFGM Test Suite', () => {
+
+    describe('Login functionality test suite', () => {
+
+        it('user should login by correct username and password', () => {
+
+        
+        });
+    
+        it('user can not login by wrong username', () => {
+    
+        });
+        
+    });
+
+   
+    
+});
