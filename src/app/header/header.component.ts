99278928fc956d614f178080e6c35c891514581e
tabay <hamide.bay@stud.hslu.ch>
2019-05-27 16:42:54 +0200

    header component done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngModule(){}
 
 }
