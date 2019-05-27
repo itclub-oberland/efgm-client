5023a826c79d1329d299f9a7c16b8cc2baf72876
tabay <hamide.bay@stud.hslu.ch>
2019-05-27 17:47:44 +0200

    added dashboard navigation

---
+++ src/app/dashboardnav/dashboardnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardnav',
+  templateUrl: './dashboardnav.component.html',
+  styleUrls: ['./dashboardnav.component.scss']
+})
+export class DashboardnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
