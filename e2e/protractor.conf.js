00ea167bcd69411c26b72b755bd097a089d0422e
javaangular1 <javaangular1@gmail.com>
2019-05-30 12:36:18 +0200

    test reporters installed

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { HtmlRepoter } = require('protractor-beautiful-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
