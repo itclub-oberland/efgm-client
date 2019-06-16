6246899e70959279e633610353765dad303ec46b
tabay <hamide.bay@stud.hslu.ch>
2019-06-17 01:46:58 +0200

    persondetail done by material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { DashboardNavComponent } from './dashboardnav/dashboardnav.component';
 import { GMComponent } from './persondetail/persondetail.component';
 import {HttpClientModule} from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,12 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
